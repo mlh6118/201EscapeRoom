9cc8669cd5823f391ab4b14c413d4d4b1ce9b40c
David Allan Turner <davidallanturner@gmail.com>
2021-11-20 18:33:32 -0700

    massive push with app to create Letter objects and test against the puzzle and the exit key is now posted for all to see as every item is completed.

--- js/winner.js
+++ js/winner.js
@@ -1,21 +1,26 @@
 'use strict'
 console.log(window.location);
 
-if(window.location.pathname == '/gameOver.html') {
-let winOrLose = document.getElementById('winOrLose');
-let replayButton = document.getElementById('replay');
+// if (window.location.pathname == '/gameOver.html') {
+ let winOrLose = document.getElementById('winOrLose');
+ let replayButton = document.getElementById('replay');
 
 
-localStorage.getItem("win")? winOrLose.innerHTML = `Congrats on your win! Your points are ${localStorage.getItem('points')}` : "Sorry for your loss";
+ if (localStorage.getItem("win") == "true") {
+  winOrLose.textContent = `Congrats on your win! Your points are ${localStorage.getItem('points')}`;
+ } else {
+  "Sorry for your loss";
+ }
 
-replayButton.addEventListener('click', function(){
+ replayButton.addEventListener('click', function () {
 
- localStorage.setItem("win", false);
- localStorage.setItem("points", 0);
- window.location.href = "/game.html";
-});
-}
+  localStorage.setItem("win", false);
+  localStorage.setItem("points", 0);
+  window.location.href = "/game.html";
+ });
 
-if(window.location.pathname == '/index.html'){
- localStorage.setItem("win", false);
-}
+// }
+
+// if (window.location.pathname == '/index.html') {
+//  localStorage.setItem("win", false);
+// }
