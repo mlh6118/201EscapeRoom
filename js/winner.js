9b0c013070b0ec16db1471bd6e88efb1439a8c10
Marni Hager <mlh6118@gmail.com>
2021-11-24 22:04:08 -0800

    Minor changes.

--- js/winner.js
+++ js/winner.js
@@ -8,57 +8,57 @@ let replayButton = document.getElementById('replay');
 
 
 if (localStorage.getItem("win") == "true") {
- winOrLose.textContent = `Congrats on your win! Your points are ${localStorage.getItem('points')}`;
+  winOrLose.textContent = `Congrats on your win! Your points are ${localStorage.getItem('points')}`;
 } else {
- winOrLose.textContent = "Sorry, you were not able to make it out in time. Please try again!";
+  winOrLose.textContent = "Sorry, you were not able to make it out in time. Please try again!";
 }
 
 replayButton.addEventListener('click', function () {
 
- localStorage.setItem("win", false);
- localStorage.setItem("points", 0);
- localStorage.setItem('initials', []);
- window.location.href = "/game.html";
+  localStorage.setItem("win", false);
+  localStorage.setItem("points", 0);
+  localStorage.setItem('initials', []);
+  window.location.href = "/game.html";
 });
 
 let maxTableSize = 5;
- // let table = document.getElementById('tbody');
+// let table = document.getElementById('tbody');
 
 
 console.log(document.getElementById('tbody').childElementCount);
 
- let table = document.getElementById('tbody');
+let table = document.getElementById('tbody');
 
- let newRow = document.createElement('tr');
- let points = document.createElement('td');
- let userName = document.createElement('td');
+let newRow = document.createElement('tr');
+let points = document.createElement('td');
+let userName = document.createElement('td');
 
- newRow.id = 'newRow';
- points.id = 'points';
- userName.id = 'initials';
-
-
+newRow.id = 'newRow';
+points.id = 'points';
+userName.id = 'initials';
 
 
 
-function sortAndPlaceAWinner(points){
- let player1Points = document.querySelector('#firstRow > td:nth-child(1)').textContent;
- let player1 = document.querySelector('#firstRow > td:nth-child(1)');
- let player2Points = document.querySelector('#secondRow > td:nth-child(1)').textContent;
- let player2 = document.querySelector('#secondRow > td:nth-child(1)');
-
- if(points< player1Points) {
-  var row = table.insertRow(0);
-
-  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-  var points = row.insertCell(0);
-  var username = row.insertCell(1);
 
 
-  points.textContent = `${localStorage.getItem('points')}`;
-  userName.textContent = `${localStorage.getItem('initials')}`;
+function sortAndPlaceAWinner(points) {
+  let player1Points = document.querySelector('#firstRow > td:nth-child(1)').textContent;
+  let player1 = document.querySelector('#firstRow > td:nth-child(1)');
+  let player2Points = document.querySelector('#secondRow > td:nth-child(1)').textContent;
+  let player2 = document.querySelector('#secondRow > td:nth-child(1)');
 
- }
+  if (points < player1Points) {
+    let row = table.insertRow(0);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    let points = row.insertCell(0);
+    let username = row.insertCell(1);
+
+
+    points.textContent = `${localStorage.getItem('points')}`;
+    userName.textContent = `${localStorage.getItem('initials')}`;
+
+  }
 }
 
 
